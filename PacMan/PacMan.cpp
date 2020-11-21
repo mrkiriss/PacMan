6fd6da5d87a360200ca10395e397785881693055
kirill <kvoskrebentsev@mail.ru>
2020-11-21 20:49:16 +0300

    FixGhostsMove

--- PacMan/PacMan.cpp
+++ PacMan/PacMan.cpp
@@ -61,10 +61,10 @@ namespace PacMan {
 		}
 	}
 
-	//функция, отвечающая за движение какого-то объекта {id, кооордината по Ох, координата по Оу, указатель на карту, новое направление, старое направление}
-	int Move(int object_id, int* x, int* y, char** map, char direction_new, char direction_old=' ') {
+	//функция, отвечающая за движение Pacman'а {кооордината по Ох, координата по Оу, указатель на карту, новое направление, старое направление}
+	int MovePacMan(int* x, int* y, char** map, char direction_new, char direction_old=' ') {
 
-		char object_name = (object_id == 0 ? 'p' : 'g'); // присвоение имени по id
+		char object_name = 'p'; // присвоение имени по id
 
 		int x_new = *x, y_new = *y;
 		int rtrn = 0; // переменная, которая возвращается - указывает на тип события после прихода на новую клетку{0 - пустота, 1 - монетка, 2 - таблетка, 3 - призрак}
@@ -115,8 +115,34 @@ namespace PacMan {
 		return rtrn;
 	}
 
-	// функция отображения карты в консоли
-	void DrawMap(char** map, int n, int m) {
+	//функция, отвечающая за движение призраков {кооордината по Ох, координата по Оу, указатель на карту, новое направление}
+	int MoveGhost(int* x, int* y, char** map, char direction_new) {
+
+		int x_new = *x, y_new = *y;
+
+		switch (direction_new) { // изменение координат по новому направлению
+		case 'N':
+			--y_new;
+			break;
+		case 'E':
+			++x_new;
+			break;
+		case 'S':
+			++y_new;
+			break;
+		case 'W':
+			--x_new;
+			break;
+		}
+		int rtrn = (map[y_new][x_new] == 'p' ? 1 : 0); // если в новой клетке стоит игрок, возвращает 1 - конец игры, если нет ирока - 0 - без изменений игрыж
+		(*x) = x_new; // изменить реальные координаты
+		(*y) = y_new;
+
+		return rtrn;
+	}
+
+	// функция отображения карты в консоли {карта, высота, ширина, координаты Х и Y пакмана и призраков}
+	void DrawMap(char** map, int n, int m, int* X, int* Y) {
 
 		// объект консоли и координат курсора
 		COORD position;
@@ -140,9 +166,9 @@ namespace PacMan {
 				case 'p':
 					block = "\x1b[31m%%\x1b[0m";
 					break;
-				case 'g':
-					block = "\x1b[35m@@\x1b[0m";
-					break;
+				//case 'g':
+				//	block = "\x1b[35m@@\x1b[0m";
+				//	break;
 				case 't':
 					bol = true;
 					block = "\x1b[32m©©\x1b[0m";
@@ -153,8 +179,20 @@ namespace PacMan {
 				default:
 					break;
 				}
+
+				// рисование призраков, без обращения к карте, чтобы сохранить значения клеток
+				if ((i == Y[1] and j == X[1]) or (i == Y[2] and j == X[2]) or (i == Y[3] and j == X[3]) or (i == Y[4] and j == X[4])) {
+					block = "\x1b[35m@@\x1b[0m";
+					bol = false;
+				}
+
 				SetConsoleCursorPosition(hConsole, position);
-				if (not bol) cout << block;
+				if (not bol) {
+					cout << block;
+				}
+				else {
+					cout << "  ";
+				}
 				position.Y++;
 				SetConsoleCursorPosition(hConsole, position);
 				cout << block;
@@ -193,9 +231,10 @@ int main(int argc, char* argv[])
 	// переменны пакмана и призраков
 		const int number_gosts = 4;
 		int score = 0;
+		int rage = 0;
 		// массивы координат пакмана - 0, призраков 1-4
-		int* array_of_coordinates_X = new int[number_gosts + 1]{14,11,11,16,16};
-		int* array_of_coordinates_Y = new int[number_gosts + 1]{23,13,13,15,15};
+		int* array_of_coordinates_X = new int[number_gosts + 1]{14,12,12,15,15};
+		int* array_of_coordinates_Y = new int[number_gosts + 1]{23,13,15,13,15};
 
 		// массивы направлений
 		char* array_of_direction_new = new char[number_gosts + 1]; // {N - вверх, E - впарво, S - вниз, W - влево}
@@ -203,10 +242,9 @@ int main(int argc, char* argv[])
 
 
 	long number_of_iteration = 0;
-
+	//PacMan::DrawMap(map, height, width, array_of_coordinates_X, array_of_coordinates_Y); // вывод карты в консоль
 	while (1) {
-
-		PacMan::DrawMap(map, height, width); // вывод карты в консоль
+		PacMan::DrawMap(map, height, width, array_of_coordinates_X, array_of_coordinates_Y); // вывод карты в консоль
 
 		//обработка нажатия клавиш для изменения направления PacMan'a
 		if (_kbhit())
@@ -220,25 +258,72 @@ int main(int argc, char* argv[])
 			}
 		}
 		int id = 0; // id активного объекта
-		PacMan::Move(id, &array_of_coordinates_X[id], &array_of_coordinates_Y[id], map, array_of_direction_new[id], array_of_direction_old[id]);
+		int situation = PacMan::MovePacMan(&array_of_coordinates_X[id], &array_of_coordinates_Y[id], map, array_of_direction_new[id], array_of_direction_old[id]); //{0 - пустота, 1 - монетка, 2 - таблетка, 3 - призрак}
+
+		switch (situation) {
+		case 1:
+			score+=10;
+			break;
+		case 2:
+			rage += 15;
+			break;
+		case 3:
+			if (rage == 0) {
+				system("cls");
+				cout << "Ты прогиграл. Ира закончена. Твоё счёт: " << score << endl;
+				exit(0);
+			}
+		}
 
 		for (id = 1; id < number_gosts+1; id++) {
 
 			vector<char> avaibale_directions; // вектор с доступными направлениями
-			int x = array_of_coordinates_Y[id], y = array_of_coordinates_Y[id];
+			int x = array_of_coordinates_X[id], y = array_of_coordinates_Y[id];
+			int x_now = array_of_coordinates_X[id], y_now = array_of_coordinates_Y[id];
 
-			if (map[x][y - 1] != 'w') avaibale_directions.push_back('N'); // проверка доступности N
-			if (map[x + 1][y] != 'w') avaibale_directions.push_back('E'); // проверка доступности E
-			if (map[x][y + 1] != 'w') avaibale_directions.push_back('S'); // проверка доступности S
-			if (map[x - 1][y] != 'w') avaibale_directions.push_back('W');// проверка доступности W
-					
-			if (avaibale_directions.size() > 2) { // если доступно >2 направлений, выбирае случайное из них, иначе не зименяем действующее
+			if (map[y - 1][x] != 'w') avaibale_directions.push_back('N'); // проверка доступности N
+			if (map[y][x + 1] != 'w') avaibale_directions.push_back('E'); // проверка доступности E
+			if (map[y + 1][x] != 'w') avaibale_directions.push_back('S'); // проверка доступности S
+			if (map[y][x - 1] != 'w') avaibale_directions.push_back('W'); // проверка доступности W
+				
+			// какая координата при движении по текущему направлению
+			switch (array_of_direction_new[id]) {
+			case 'N':
+				--y_now;
+				break;
+			case 'E':
+				++x_now;
+				break;
+			case 'S':
+				++y_now;
+				break;
+			case 'W':
+				--x_now;
+				break;
+			}
+
+			if (avaibale_directions.size() > 2 or map[y_now][x_now]=='w') { // если доступно >2 (выход на развилку) направлений или предыдущее направление уже не доступно, выбирае случайное из них, иначе не заменяем текущее
 				array_of_direction_new[id] = avaibale_directions[rand() % avaibale_directions.size()];
 			}
 
-			PacMan::Move(id, &x, &y, map, array_of_direction_new[id]);
+			if (PacMan::MoveGhost(&x, &y, map, array_of_direction_new[id]) == 1) {
+				if (rage == 0) { // столкновение с p без рейджа - конец игры
+					system("cls");
+					cout << "Ты прогиграл. Ира закончена. Твоё счёт: " << score << endl;
+					exit(0);
+				}
+				else { // столкновение с p с рейджом - бонусные очки [смерть призрака не риализована]
+					score += 200;
+				}
+			}
+
+			array_of_coordinates_X[id] = x, array_of_coordinates_Y[id] = y;
+			avaibale_directions.clear();
+
 		}
 
 		number_of_iteration++;
+		if (rage>0) rage--;
+		//system("cls");
 	}
 }
