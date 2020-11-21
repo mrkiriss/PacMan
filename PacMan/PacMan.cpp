24e4eee4b4b74ccc0ad9b42e63f498ac5a78c8cd
kirill <kvoskrebentsev@mail.ru>
2020-11-21 18:43:05 +0300

    AddAllMove

--- PacMan/PacMan.cpp
+++ PacMan/PacMan.cpp
@@ -15,15 +15,6 @@ using namespace std;
 
 namespace PacMan {
 
-	//функция, выводящая информацию о ошибке по её id
-	void IndicateError(int id) {
-		switch (id) {
-		case 1: //нажатие не на клавиши стрелок
-			cout << "Управлять PacMan'ом возможно только с помощью клавиш-стрелок" << endl;
-			break;
-		}
-	}
-
 	//функция, преобразовывающая id клавиши в направление движения Pacman'а
 	char EditDirectionForPacman(int i) {
 		if (i == 224) {
@@ -52,58 +43,76 @@ namespace PacMan {
 	}
 
 	// функция, выполняющая определённое действие, после входа в данную клетку
-	void ProcessCellAfterMove(int x, int y, char** map) {
-		cout << "pass" << endl;
+	int ProcessCellAfterMove(int x, int y, char** map) {
+		switch (map[x][y]) {
+			// {0 - пустота, 1 - монетка, 2 - таблетка, 3 - призрак}
+		case ' ':
+			return 0;
+			break;
+		case 'c':
+			return 1;
+			break;
+		case 't':
+			return 2;
+			break;
+		case 'g':
+			return 3;
+			break;
+		}
 	}
 
-	//функция, отвечающая за движение какого-то объекта {имя, новое направление, старое направление, кооордината по Ох, координата по Оу, указатель на карту}
-	void Move(char object_name, char direction_new, char direction_old, int* x, int* y, char** map) {
+	//функция, отвечающая за движение какого-то объекта {id, кооордината по Ох, координата по Оу, указатель на карту, новое направление, старое направление}
+	int Move(int object_id, int* x, int* y, char** map, char direction_new, char direction_old=' ') {
+
+		char object_name = (object_id == 0 ? 'p' : 'g'); // присвоение имени по id
+
 		int x_new = *x, y_new = *y;
-		switch (direction_new) {
+		int rtrn = 0; // переменная, которая возвращается - указывает на тип события после прихода на новую клетку{0 - пустота, 1 - монетка, 2 - таблетка, 3 - призрак}
+
+		switch (direction_new) { // изменение координат по новому направлению
 		case 'N':
-			++y_new;
+			--y_new;
 			break;
 		case 'E':
 			++x_new;
 			break;
 		case 'S':
-			--y_new;
+			++y_new;
 			break;
 		case 'W':
 			--x_new;
 			break;
 		}
 		if (map[y_new][x_new] != 'w') { // клетка по новому направлению доступна (это не стена)
-			ProcessCellAfterMove(y_new, x_new, map);
+			rtrn = ProcessCellAfterMove(y_new, x_new, map);
 			map[y_new][x_new] = object_name;
 			map[*y][*x] = ' ';
 		}
 		else {
 			x_new = *x, y_new = *y;
-			switch (direction_old) {
+			switch (direction_old) { // изменение координат по старому направлению
 			case 'N':
-				++y_new;
+				--y_new;
 				break;
 			case 'E':
 				++x_new;
 				break;
 			case 'S':
-				--y_new;
+				++y_new;
 				break;
 			case 'W':
 				--x_new;
 				break;
 			}
 			if (map[y_new][x_new] != 'w') { // клетка доступна только по старому направлению
-				ProcessCellAfterMove(y_new, x_new, map);
+				rtrn = ProcessCellAfterMove(y_new, x_new, map);
 				map[y_new][x_new] = object_name;
 				map[*y][*x] = ' ';
-
-				//return map;
 			} // иначе ничего не делать, т.к движение по обоим направлениям недоступно
 		}
 		*x = x_new; // изменить реальные координаты
 		*y = y_new;
+		return rtrn;
 	}
 
 	// функция отображения карты в консоли
@@ -115,27 +124,28 @@ namespace PacMan {
 		position.Y = 2;
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		string block = "";
-		boolean coin = false;
+		boolean bol = false; // условие особого вывода
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				SetConsoleCursorPosition(hConsole, position);
 				switch (map[i][j]) {
 				case 'w':
-					block = "==";
+					block = "\x1b[34m##\x1b[0m";
 					break;
 				case 'c':
-					block = "$$";
-					coin = true;
+					block = "\x1b[33m$$\x1b[0m";
+					bol = true;
 					break;
 				case 'p':
-					block = "66";
+					block = "\x1b[31m%%\x1b[0m";
 					break;
 				case 'g':
-					block = "@@";
+					block = "\x1b[35m@@\x1b[0m";
 					break;
 				case 't':
-					block = "**";
+					bol = true;
+					block = "\x1b[32m©©\x1b[0m";
 					break;
 				case ' ':
 					block = "  ";
@@ -144,87 +154,91 @@ namespace PacMan {
 					break;
 				}
 				SetConsoleCursorPosition(hConsole, position);
-				if (not coin) cout << block;
+				if (not bol) cout << block;
 				position.Y++;
 				SetConsoleCursorPosition(hConsole, position);
 				cout << block;
 				position.Y--;
 				position.X += 3;
-				coin = false;
+				bol = false;
 			}
 			position.Y += 2;
 			position.X = 2;
 		}
 	}
+
 }
 
 int main(int argc, char* argv[])
 {
 	setlocale(LC_ALL, "Russian_Russia.1251");
-
-	//переменные пакмана
-	vector<string> texture;
-	int score = 0;
-	char direction_new = ' '; // {N - вверх, E - впарво, S - вниз, W - влево}
-	char direction_old = ' ';
+	std::srand(time(NULL));
 
 	//переменные карты
-	int height = 31, width = 28;// размер карты
+		const int height = 31, width = 28;// размер карты
+		ifstream in("map.txt"); //файл с картой
+		string line = ""; // контейнер строки из файла карты
 
-	ifstream in("C:\\prog\\MyProjects\\PacMan\\PacMan\\map.txt"); //файл с картой
-	string line = ""; // контейнер строки из файла карты
+		char** map = new char* [height]; // двумерный массив с картой __ {p - пакман, w - стена, c - монетка, t - таблетка, g - призрак}
 
-	// {p - пакман, w - стена, c - монетка, t - таблетка, g - призрак}
-	char** map = new char* [height]; // двумерный массив с картой
-
-	for (int i = 0; i < height; i++) { // заполнение массива с картой
-		if (not in.is_open()) break;
-		map[i] = new char[width];
-		getline(in, line);
-		for (int j = 0; j < width; j++) {
-			map[i][j] = line[j];
+		for (int i = 0; i < height; i++) { // заполнение массива с картой
+			if (not in.is_open()) break;
+			map[i] = new char[width];
+			getline(in, line);
+			for (int j = 0; j < width; j++) {
+				map[i][j] = line[j];
+			}
 		}
-	}
-	in.close();
+		in.close();
+	// переменны пакмана и призраков
+		const int number_gosts = 4;
+		int score = 0;
+		// массивы координат пакмана - 0, призраков 1-4
+		int* array_of_coordinates_X = new int[number_gosts + 1]{14,11,11,16,16};
+		int* array_of_coordinates_Y = new int[number_gosts + 1]{23,13,13,15,15};
+
+		// массивы направлений
+		char* array_of_direction_new = new char[number_gosts + 1]; // {N - вверх, E - впарво, S - вниз, W - влево}
+		char* array_of_direction_old = new char[1];
 
 
-	//переменные призраков
-	int ghost1_x, ghost1_y, ghost2_x, ghost2_y, ghost3_x, ghost3_y, ghost4_x, ghost4_y;
+	long number_of_iteration = 0;
 
-
-	PacMan::DrawMap(map, height, width);
-
-	/*
-	COORD position;
-	position.X = 2;
-	position.Y = 2;
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	for (int i = 0; i < 2; i++) {
-		SetConsoleCursorPosition(hConsole, position);
-		cout << "##";
-		position.Y++;
-		SetConsoleCursorPosition(hConsole, position);
-		cout << "##";
-		position.Y--;
-		position.X+=2;
-	}
-	*/
-
-
-	while (0) {
+	while (1) {
 
 		PacMan::DrawMap(map, height, width); // вывод карты в консоль
-		//обработка нажатия клавиш
+
+		//обработка нажатия клавиш для изменения направления PacMan'a
 		if (_kbhit())
 		{
 			int i = _getch();
 			char ans = PacMan::EditDirectionForPacman(i);
 			if (ans != ' ') {
-				direction_old = direction_new;
-				direction_new = ans;
+				array_of_direction_old[0] = array_of_direction_new[0];
+				array_of_direction_new[0] = ans;
 				//cout << direction_new << ' ' << direction_old<<endl;
 			}
 		}
+		int id = 0; // id активного объекта
+		PacMan::Move(id, &array_of_coordinates_X[id], &array_of_coordinates_Y[id], map, array_of_direction_new[id], array_of_direction_old[id]);
 
+		for (id = 1; id < number_gosts+1; id++) {
+
+			vector<char> avaibale_directions; // вектор с доступными направлениями
+			int x = array_of_coordinates_Y[id], y = array_of_coordinates_Y[id];
+
+			if (map[x][y - 1] != 'w') avaibale_directions.push_back('N'); // проверка доступности N
+			if (map[x + 1][y] != 'w') avaibale_directions.push_back('E'); // проверка доступности E
+			if (map[x][y + 1] != 'w') avaibale_directions.push_back('S'); // проверка доступности S
+			if (map[x - 1][y] != 'w') avaibale_directions.push_back('W');// проверка доступности W
+					
+			if (avaibale_directions.size() > 2) { // если доступно >2 направлений, выбирае случайное из них, иначе не зименяем действующее
+				array_of_direction_new[id] = avaibale_directions[rand() % avaibale_directions.size()];
+			}
+
+			PacMan::Move(id, &x, &y, map, array_of_direction_new[id]);
+		}
+
+		number_of_iteration++;
 	}
 }
